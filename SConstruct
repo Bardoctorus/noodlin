89f34b362fd8740a1f03bbdd25437537160c9ee9
Bardoctorus <ian.m.buckley@gmail.com>
2026-03-03 14:17:23 +0100

    project setup and quick test at runtime child node instantiation

--- SConstruct
+++ SConstruct
@@ -5,7 +5,7 @@ import sys
 from methods import print_error
 
 
-libname = "EXTENSION-NAME"
+libname = "noodlin-lib"
 projectdir = "project"
 
 localEnv = Environment(tools=["default"], PLATFORM="")
--- project/example.gd
+++ project/example.gd
@@ -1,6 +1,13 @@
 extends Node
 
+var aud = AudioStreamPlayer.new()
 
 func _ready() -> void:
 	var example := ExampleClass.new()
 	example.print_type(example)
+	addting()
+
+
+func addting():
+	add_child(aud)
+	pass
